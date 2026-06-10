a50f754725c54eee9cd359bf992f89bb6fbb2c48
Sp8ceranger <Spacerangerdidi@outlook.com>
2026-06-10 07:55:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,34 @@
-//% color=#ff8800 icon="\\uf1d9" block="Arduino Link"
-
+//% color=#ff8800 icon="\uf1d9" block="Arduino Link"
 namespace ArduinoLink {
 
-    //% block="initialiser communication Arduino"
+    let lastLine = ""
 
-    export function init() {
-
-        serial.redirect(
-
-            SerialPin.P0,
-
-            SerialPin.P1,
-
-            BaudRate.BaudRate9600
-
-        )
-
+    //% block="initialiser communication Arduino TX %tx RX %rx à %baud bauds"
+    //% tx.defl=SerialPin.P0
+    //% rx.defl=SerialPin.P1
+    //% baud.defl=BaudRate.BaudRate9600
+    export function init(tx: SerialPin, rx: SerialPin, baud: BaudRate) {
+        serial.redirect(tx, rx, baud)
     }
 
     //% block="envoyer à Arduino %msg"
-
     export function send(msg: string) {
-
         serial.writeLine(msg)
-
     }
 
     //% block="lire message Arduino"
-
     export function receive(): string {
-
-        return serial.readLine()
-
+        lastLine = serial.readLine()
+        return lastLine
     }
 
+    //% block="%text est contenu dans la dernière ligne"
+    export function contains(text: string): boolean {
+        return lastLine.includes(text)
+    }
+
+    //% block="message Arduino disponible ?"
+    export function messageDisponible(): boolean {
+        return serial.available() > 0
+    }
 }
