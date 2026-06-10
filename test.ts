4eccb775d235fbbcfd789ef840fe07ffb0f7de4d
Sp8ceranger <Spacerangerdidi@outlook.com>
2026-06-10 14:24:31 +0200

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,9 @@
+﻿arduinoUART.init(SerialPin.P0, SerialPin.P1, BaudRate.BaudRate9600)
+arduinoUART.sendText("bonjour")
+arduinoUART.sendNumber(42)
+arduinoUART.sendKeyValue("LED", "1")
+
+let message = arduinoUART.readLine()
+if (arduinoUART.messageEquals(message, "OK")) {
+    basic.showIcon(IconNames.Yes)
+}
